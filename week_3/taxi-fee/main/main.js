944838786fb004ac001ba8370911da297b042cf2
wenyuntian <157325986@qq.com>
2019-03-05 20:10:38 +0800

    pass the test when the distance <= 2km and wait 5.5 minute

--- week_3/taxi-fee/main/main.js
+++ week_3/taxi-fee/main/main.js
@@ -1,5 +1,11 @@
 module.exports = function main(distance, waitingTime) {
+    let totalFee = 0;
+
     if(distance <= 2 && waitingTime === 0) {
-        return 6
+        totalFee = 6
+    } else if (distance >= 2 && waitingTime !== 0) {
+        totalFee = 6 + (distance - 2) * 0.8 +  waitingTime * 0.25
     }
+
+    return Math.round(totalFee);
 };
--- week_3/taxi-fee/spec/main-spec.js
+++ week_3/taxi-fee/spec/main-spec.js
@@ -1,11 +1,19 @@
 const main = require('../main/main');
 
 describe('taxi fee', function () {
-    it ('should give ￥6 when the distance >= 2km and wait 0 minute', () => {
+    it ('should give ￥6 when the distance <= 2km and wait 0 minute', () => {
         let distance = 1.5;
         let waitingTime = 0;
         let result = main(distance, waitingTime)
 
         expect(result).toBe(6)
+    })
+
+    it('should give ￥ when the distance <= 2km and wait 5.5 minute', () => {
+        let distance = 6.5;
+        let waitingTime = 5.5;
+        let result = main(distance, waitingTime)
+
+        expect(result).toBe(11)
     })
 });

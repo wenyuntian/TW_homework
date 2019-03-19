193e15cf9e125b546f4279950892d752e3b30490
wenyuntian <157325986@qq.com>
2019-03-19 14:49:59 +0800

    the refactoring version

--- week_4/take-out-food/src/best-charge.js
+++ week_4/take-out-food/src/best-charge.js
@@ -14,16 +14,48 @@ function bestCharge(selectedItems) {
   return printOrderDetail(orderDetail);
 }
 
-function buildFoodInformationList(selectedItems, allFoodItems, orderDetail) {
+function validateInput(selectedItems) {
+  let selectedObjects = [];
+  let regular = /^(ITEM\d{4}\sx\s\d)$/;
 
   selectedItems.forEach((selectedItem) => {
-    let [itemId, itemAmount] = selectedItem.split(' x ');
+    if (regular.test(selectedItem)) {
+      selectedObjects.push({
+        itemId: selectedItem.split(' x ')[0],
+        itemAmount: selectedItem.split(' x ')[1]
+      })
+    } else {
+      throw new Error('Illegal input');
+    }
+  })
+  return selectedObjects;
+}
+
+function sortPromotionList(PromotionList) {
+  PromotionList.sort((prev, next) => {
+    return next.discounts - prev.discounts;
+  })
+}
+
+function buildBillImformation(orderDetail) {
+  orderDetail.savedMoney = orderDetail.promotionList[0].discounts;
+  orderDetail.totalMoney -= orderDetail.savedMoney;
+}
+
+function buildFoodInformationList(selectedItems, allFoodItems, orderDetail) {
+  let selectedObjects = validateInput(selectedItems);
+
+  selectedObjects.forEach((selectedItem) => {
+    let {
+      itemId,
+      itemAmount
+    } = selectedItem;
 
     allFoodItems.forEach((foodItem) => {
-      if(itemId === foodItem.id) {
+      if (itemId === foodItem.id) {
         orderDetail.foodInformationList.set(itemId, {
           amount: itemAmount,
-          ... foodItem
+          ...foodItem
         })
         orderDetail.totalMoney += foodItem.price * itemAmount;
       }
@@ -31,50 +63,56 @@ function buildFoodInformationList(selectedItems, allFoodItems, orderDetail) {
   })
 }
 
-function buildDiscountInformationList(promotions, orderDetail) {
-  // 满30减6元
-  {
-    if(orderDetail.totalMoney >= 30) {
-      orderDetail.promotionList.push({
-        type: promotions[0].type,
-        discounts: 6,
-        note: ''
-      })
-    }
-  }
+// 满30减6元
+function FullReductionPromotion(promotions, orderDetail) {
 
-  // 指定菜品半价
-  {
-    let promotionItems = promotions[1].items;
-    let discounts = 0;
-    let note = '';
-
-    orderDetail.foodInformationList.forEach((foodInformation, foodId) => {
-      promotionItems.forEach((promotionItem) => {
-        if(promotionItem === foodId) {
-          discounts += foodInformation.amount * foodInformation.price * 0.5;
-          note += (note ? `，${foodInformation.name}` : foodInformation.name);
-        }
-      })
-    })
-
+  if (orderDetail.totalMoney >= 30) {
     orderDetail.promotionList.push({
-      type: promotions[1].type,
-      discounts: discounts,
-      note: note
+      type: promotions[0].type,
+      discounts: 6,
+      note: ''
     })
   }
+}
 
-  orderDetail.promotionList.sort((prev, next) => {
-    return next.discounts - prev.discounts;
+// 指定商品半价
+function halfPricePromotion(promotions, orderDetail) {
+  let promotionItems = promotions[1].items;
+  let discounts = 0;
+  let note = '';
+
+  orderDetail.foodInformationList.forEach((foodInformation, foodId) => {
+    promotionItems.forEach((promotionItem) => {
+      if (promotionItem === foodId) {
+        discounts += foodInformation.amount * foodInformation.price * 0.5;
+        note += (note ? `，${foodInformation.name}` : foodInformation.name);
+      }
+    })
   })
 
-  orderDetail.savedMoney = orderDetail.promotionList[0].discounts;
-  orderDetail.totalMoney -= orderDetail.savedMoney
+  orderDetail.promotionList.push({
+    type: promotions[1].type,
+    discounts: discounts,
+    note: note
+  })
+}
+
+function buildDiscountInformationList(promotions, orderDetail) {
+
+  FullReductionPromotion(promotions, orderDetail);
+  halfPricePromotion(promotions, orderDetail);
+
+  sortPromotionList(orderDetail.promotionList);
+  buildBillImformation(orderDetail);
 }
 
 function printOrderDetail(orderDetail) {
-  let {foodInformationList, promotionList, totalMoney, savedMoney} = orderDetail;
+  let {
+    foodInformationList,
+    promotionList,
+    totalMoney,
+    savedMoney
+  } = orderDetail;
   let orderDetailString = '============= 订餐明细 =============\n';
 
   foodInformationList.forEach((foodItem) => {
@@ -83,6 +121,6 @@ function printOrderDetail(orderDetail) {
 
   orderDetailString += savedMoney === 0 ? '' : `-----------------------------------\n使用优惠:\n${promotionList[0].type}${promotionList[0].note ? `(${promotionList[0].note})` : ''}，省${orderDetail.savedMoney}元\n`;
   orderDetailString += `-----------------------------------\n总计：${orderDetail.totalMoney}元\n===================================`
-  
+
   return orderDetailString
 }
